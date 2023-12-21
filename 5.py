c66c3751352f4b4ec4f3d47945f988e05759bb97
ankitg-02 <146383416+ankitg-02@users.noreply.github.com>
2023-12-21 22:37:02 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,7 @@
+x1=int(input("enter x coordinate:"))
+x2=int(input("enter x coordinate:"))
+y1=int(input("enter y coordinate:"))
+y2=int(input("enter y coordinate:"))
+d=((x1-x2)**2+(y1-y2)**2)**0.5
+print("distance:",d)
+
